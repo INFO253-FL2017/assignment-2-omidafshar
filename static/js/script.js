c278f8358bcbc220f05387e6142b875cc62012b6
Omid Afshar <omidafshar@berkeley.edu>
2017-10-10 15:16:42 -0700

    Configured website on server

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,58 @@
-// script.js
+function allFieldsFilled() {
 
-console.log("js/script.js has been loaded!");
+	var nameField = document.getElementById("name").value;
+	var subjectField = document.getElementById("subject").value;
+	var messageField = document.getElementById("message").value;
+
+	if (document.body.children[0].nodeName == "HEADER") {
+
+		var messageDiv = document.createElement("div");
+		messageDiv.id = "message-div";
+		document.body.insertBefore(messageDiv,document.body.firstChild);
+
+	}
+
+	var messageNode = document.getElementById("message-div");
+
+	while (messageNode.hasChildNodes()) {
+    	messageNode.removeChild(messageNode.lastChild);
+	}
+
+	if (nameField == "") {
+
+		var p = document.createElement("p");
+		p.innerHTML = "Name field must be completed";
+		messageNode.appendChild(p);
+
+	}
+
+	if (subjectField == "") {
+
+		var q = document.createElement("p");
+		q.innerHTML = "Subject field must be completed";
+		messageNode.appendChild(q);
+
+	}
+
+	if (messageField == "") {
+
+		var r = document.createElement("p");
+		r.innerHTML = "Message field must be completed";
+		messageNode.appendChild(r);
+
+	}
+
+	if (document.getElementById("message-div").innerHTML == "") {
+
+		var s = document.createElement("p");
+		s.innerHTML = "Your message has been sent";
+		messageNode.appendChild(s);
+
+	}
+
+	event.preventDefault();
+
+}
+
+var form = document.getElementById("contact-form");
+form.addEventListener('submit', allFieldsFilled);
--- webserver.py
+++ webserver.py
@@ -17,3 +17,52 @@ def hello_world():
     returns will be sent to the browser
     """
     return render_template("index.html") # Render the template located in "templates/index.html"
+
+@app.route('/index')
+def home_page():
+    """
+    If someone goes to the root of your website (i.e. http://localhost:5000/index), run this function. The string that this
+    returns will be sent to the browser
+    """
+    return render_template("index.html") # Render the template located in "templates/index.html" 
+
+@app.route('/about')
+def about_page():
+   
+    return render_template("about_us.html")
+
+
+@app.route('/contact')
+def contact_page():
+    
+    return render_template("contact_us.html")
+
+
+@app.route('/blog/8-experiments-in-motivation')
+def first_blog():
+
+    return render_template("blog/Post1.html")
+
+
+@app.route('/blog/a-mindful-shift-of-focus')
+def second_blog():
+
+    return render_template("blog/Post2.html")
+
+
+@app.route('/blog/how-to-develop-an-awesome-sense-of-direction')
+def third_blog():
+
+    return render_template("blog/Post3.html")
+
+
+@app.route('/blog/training-to-be-a-good-writer')
+def fourth_blog():
+
+    return render_template("blog/Post4.html")
+
+
+@app.route('/blog/what-productivity-systems-wont-solve')
+def fifth_blog():
+
+    return render_template("blog/Post5.html")                                
